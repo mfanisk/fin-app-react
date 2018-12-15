def0a2a989a10cc7b83bd2b68e996b068eb81247
Fanis Kallangodan <kiddevil@Faniss-MacBook-Air.local>
2018-12-15 20:50:44 +0530

    Environment setup done

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from "react";
+import HelloWorld from "fin-app-react/HelloWorld";
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Viewers!" />;
+}
